81e935a052e2e14643b447020de95beb5720e134
IlDavidone <144787644+IlDavidone@users.noreply.github.com>
2025-04-15 23:04:20 +0200

    Added all parameters to create game cards

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 const nameInput = document.querySelector(".game");
+const coverInput = document.querySelector(".src");
 const playtimeInput = document.querySelector(".playtime");
 const genreInput = document.querySelector(".genre");
 const platform = document.querySelector(".platform");
-const checkStatus = document.querySelector(".played");
+const checkStatus = document.querySelector(".status");
+const checkPlatform = document.querySelector(".platform");
 const addButton = document.querySelector(".add");
 const inputContainer = document.querySelector(".input-container");
+const contentDiv = document.querySelector(".content");
 const closeButton = document.querySelector(".close");
 const testDiv = document.querySelector(".test");
 
-let gameArr = [], gameUid = [], bookElement, inputMenuOpened = 0;
+let gameArr = [], gameUid = [], gameCardArr = [], inputMenuOpened = 0;
 
-function Game(name, playtime, genre, played) {
+function Game(name, playtime, genre, played, platform, cover) {
     this.name = name;
     this.playtime = playtime;
     this.genre = genre;
     this.played = played;
+    this.platform = platform;
+    this.cover = cover;
 }
 
 Game.prototype.getUid = function() {
@@ -23,14 +28,24 @@ Game.prototype.getUid = function() {
 }
 
 Game.prototype.createElement = function() {
-    bookElement = document.createElement("p");
-    bookElement.textContent = `${this.name}, ${this.playtime}, ${this.genre}, ${platform.options[platform.selectedIndex].text}`;
-    testDiv.appendChild(bookElement);
+    let gameCard = document.createElement("div");
+    let gameCardCover = document.createElement("img");
+    if (gameCardCover =! "") {
+        gameCardCover.src =`${this.cover}`;
+    }
+
 }
+
+//var text = e.options[e.selectedIndex].text;
 
 const Button = document.querySelector(".add-button");
 Button.addEventListener("click", () => {
-    let game = new Game(nameInput.value, playtimeInput.value, genreInput.value);
+    let game = new Game(nameInput.value, 
+        playtimeInput.value, 
+        genreInput.value, 
+        checkStatus.options[checkStatus.selectedIndex].text, 
+        checkPlatform.options[checkPlatform.selectedIndex].text, 
+        coverInput.value);
     game.getUid();
     game.createElement();
     gameArr.push(game);
@@ -38,8 +53,8 @@ Button.addEventListener("click", () => {
 
 addButton.addEventListener("click", () => {
     inputContainer.classList.add("opened");
-})
+});
 
 closeButton.addEventListener("click", () => {
     inputContainer.classList.remove("opened");
-})
+});
